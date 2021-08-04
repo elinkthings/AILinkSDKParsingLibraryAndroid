81cb7d7f918428d422925f1fdf85a9ab65d466d2
xing <573676740@qq.com>
2021-08-04 16:13:45 +0800

    1,修复找不到so库的问题

--- app/src/main/java/aicare/net/cn/sdk/ailinkbleparsinglibrary/MainActivity.java
+++ app/src/main/java/aicare/net/cn/sdk/ailinkbleparsinglibrary/MainActivity.java
@@ -1,8 +1,11 @@
 package aicare.net.cn.sdk.ailinkbleparsinglibrary;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import cn.net.aicare.algorithmutil.BodyFatData;
+import cn.net.aicare.modulelibrary.module.utils.AicareBleConfig;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BodyFatData bodyFatData = AicareBleConfig.getBodyFatData(0, 1, 25, 65, 170, 500);
+        Log.i("TAG",bodyFatData.toString());
     }
 }
