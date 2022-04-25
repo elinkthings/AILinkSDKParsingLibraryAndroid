94ca755c334a0e7ebb81623f0bcd3e25e0da6230
陈福行 <chenfuxing@elinkthings.com>
2022-04-25 18:07:08 +0800

    1,update data

--- app/src/main/java/aicare/net/cn/sdk/ailinkbleparsinglibrary/MainActivity.java
+++ app/src/main/java/aicare/net/cn/sdk/ailinkbleparsinglibrary/MainActivity.java
@@ -3,6 +3,8 @@ package aicare.net.cn.sdk.ailinkbleparsinglibrary;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.besthealth.bhBodyComposition120.BhBodyComposition;
+
 import androidx.appcompat.app.AppCompatActivity;
 import cn.net.aicare.algorithmutil.BodyFatData;
 import cn.net.aicare.modulelibrary.module.utils.AicareBleConfig;
@@ -15,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BodyFatData bodyFatData = AicareBleConfig.getBodyFatData(0, 1, 25, 65, 170, 500);
         Log.i("TAG",bodyFatData.toString());
+        Log.i("TAG", new BhBodyComposition().getBodyComposition()+"");
     }
 }
